72e5c63f8cf167a0d43850634fdad9f11715dafc
Ivan-Pchelin <45098892+Ivan-Pchelin@users.noreply.github.com>
2019-12-12 15:33:25 +0300

    Add files via upload

--- tabletogenemapper.py
+++ tabletogenemapper.py
@@ -1,32 +1,31 @@
-# This script transforms a TAB-delimited table with microsatellite polymorphism data
-# into GeneMapper file format
+# This script transforms a TAB-delimited table with haploid microsatellite polymorphism data
+# into GeneMapper file format, suitable for R poppr and polysat packages
 
-# Version 2019-01-16
+# Version 2019-12-12 by Ivan Pchelin arcella.oraia@gmail.com
 
-# Соберем множество с локусами вносимых данных и проверим, что новых нет
 import re
-# Заберем заголовок infile в список
 with open ('infile.txt') as inf:
-	line = inf.readline()
-	print(line)
-	elements = re.findall(r'\S+', line.strip())
+    line = inf.readline()
+    print(line)
+    elements = re.findall(r'\S+', line.strip())
 loci = []
 for i in elements:
-	loci.append(i)
+    loci.append(i)
 with open ('infile.txt') as inf:
-	data = inf.readlines()
+    data = inf.readlines()
 
 with open ('GeneMapper.txt', 'w') as ouf:
-	switch = 0 # Переключатель по локусам
-	while switch < len(loci):
-		runner = 1
-		while runner < len(data):
-			elements = re.split('\t', data[runner])
-			ouf.write(elements[0])
-			ouf.write('\t')
-			ouf.write(loci[switch])
-			ouf.write('\t')
-			ouf.write(elements[switch+1].strip())
-			ouf.write('\n')
-			runner += 1
-		switch += 1
+    ouf.write('Sample.Name\tMarker\tAllele.1\tAllele.2\n')
+    switch = 0
+    while switch < len(loci):
+        runner = 1
+        while runner < len(data):
+            elements = re.split('\t', data[runner])
+            ouf.write(elements[0])
+            ouf.write('\t')
+            ouf.write(loci[switch])
+            ouf.write('\t')
+            ouf.write(elements[switch+1].strip())
+            ouf.write('\t\n')
+            runner += 1
+        switch += 1
